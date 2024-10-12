e81e424fbafd546c7ff5fa2caa96439dcb6de9e7
Amohamed24 <mmohameda97@gmail.com>
2024-10-12 15:09:21 -0500

    added functions for basic math operators

--- main.js
+++ main.js
@@ -11,13 +11,12 @@ const button = document.querySelectorAll("button");
 // add click event listener to update display
 button.forEach((button) => {
     button.addEventListener('click', () => {
-        if (input.length < 7) {
+        if (input.length < 9) {
             input += button.id
             updateDisplay(input)
         }
     })
 })
-
 
 function updateDisplay(value) {
     display.innerHTML = value
@@ -39,27 +38,93 @@ function clearDisplay() {
 
 
 
-const plusbtn = document.querySelector("+")
+const plusbtn = document.querySelector("#plus");
 plusbtn.addEventListener('click', () => {
     addNumbers()
+    display.innerHTML = `${sum}`
+})
+
+const minusbtn = document.querySelector("#minus");
+minusbtn.addEventListener('click', () => {
+    minusNumbers()
+    display.innerHTML = `${diff}`
+})
+
+const timesbtn = document.querySelector("#times");
+timesbtn.addEventListener('click', () => {
+    timesNumbers()
+    display.innerHTML = `${multi}`
+})
+
+const dividebtn = document.querySelector("#divide");
+dividebtn.addEventListener('click', () => {
+    divideNumbers()
+    display.innerHTML = `${split}`
 })
 
 // function to add
-function addFunc() {
-    // i want to store number entered until + is pressed
-    // and nums after it
-    // do sum of both or more nums
+function addNumbers() {
+    let num1 = parseInt(prompt("Please enter a number less than 100: "))
+    let num2 = parseInt(prompt("Please enter a number less than 100: "))
+    if (num1 & num2 & num1<100 & num2 <100) {
+        sum = num1 + num2
+        return alert(`${num1} added by ${num2} equals ${sum}`);
+    } else {
+        alert("Invalid choices!")
+        display.innerHTML = "error"
+    }
 }
 
+// function to subtract
+function minusNumbers() {
+    let num1 = parseInt(prompt("Please enter a number less than 100: "))
+    let num2 = parseInt(prompt("Please enter a number less than 100: "))
+    if (num1 & num2 & num1<100 & num2 <100) {
+        diff = num1 - num2
+        return alert(`${num1} subtracted by ${num2} equals ${diff}`);
+    } else {
+        alert("Invalid choices!")
+        display.innerHTML = "error"
+    }
+}
+
+// function to multiply
+function timesNumbers() {
+    let num1 = parseInt(prompt("Please enter a number less than 100: "))
+    let num2 = parseInt(prompt("Please enter a number less than 100: "))
+    if (num1 & num2 & num1<100 & num2 <100) {
+        multi = num1 * num2
+        return alert(`${num1} multiplied by ${num2} equals ${multi}`);
+    } else {
+        alert("Invalid choices!")
+        display.innerHTML = "error"
+    }
+}
+
+// function to divide
+function divideNumbers() {
+    let num1 = parseInt(prompt("Please enter a number less than 100: "))
+    let num2 = parseInt(prompt("Please enter a number less than 100: "))
+    if (num1 & num2 & num1<100 & num2 <100) {
+        split = num1 / num2
+        return alert(`${num1} divided by ${num2} equals ${split}`);
+    } else {
+        alert("Invalid choices!")
+        display.innerHTML = "error"
+    }
+}
 
 
 
 const equalbtn = document.querySelector("=")
 equalbtn.addEventListener('click', () => {
-    returnAns()
+    operate()
 })
 
 
-function returnAns() {
-    
+function operate() {
+    // takes an operator and 2 numbers
 }
+
+
+
